43df7d93bfde082b5e313a2ec3e79dcb34035d36
James Sapp <jameseisapp@gmail.com>
2020-12-09 15:48:23 -0600

    commit tree 65f2441905e5f5b7ec311b02e73e33822a5d8861

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :color, :size, :material, :condition #setters & getters
+    attr_reader :brand #brand getter
+
+    def initialize(brand) #brand setter
+        @brand = brand 
+    end
+
+    def cobble #instance method 
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end 
+end 
+
+shoe = Shoe.new("Louis Vuitton")
+shoe.cobble
